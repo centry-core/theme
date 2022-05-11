6d0e934f808792035c9610789e9d732769e8c6eb
Mykhailo_Hunko <mykhailo.hunko@gmail.com>
2022-05-11 18:59:29 +0300

    Update API/models/slots for Backend performance tests

---
+++ slots/sources.py
@@ -0,0 +1,36 @@
+from pylon.core.tools import web  # pylint: disable=E0611,E0401
+from tools import auth  # pylint: disable=E0401
+
+
+class Slot:  # pylint: disable=E1101,R0903
+    @web.slot('sources_content')
+    def params_table_content(self, context, slot, payload):
+        """
+        sources with payload config:
+
+        """
+        from pylon.core.tools import log
+        log.info('slot: [%s], payload: %s', slot, payload)
+        with context.app.app_context():
+            return self.descriptor.render_template(
+                'part/sources/content.html',
+                **payload
+            )
+
+    @web.slot('sources_scripts')
+    def params_table_scripts(self, context, slot, payload):
+        from pylon.core.tools import log
+        log.info('slot: [%s], payload: %s', slot, payload)
+        with context.app.app_context():
+            return self.descriptor.render_template(
+                'part/sources/scripts.html',
+            )
+
+    @web.slot('sources_styles')
+    def params_table_styles(self, context, slot, payload):
+        from pylon.core.tools import log
+        log.info('slot: [%s], payload: %s', slot, payload)
+        with context.app.app_context():
+            return self.descriptor.render_template(
+                'part/sources/styles.html',
+            )
