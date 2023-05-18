2b4a414fec8e3b8c4505a0ad4449b0ab513e2858
Aspect13 <ad13box@gmail.com>
2023-05-18 20:30:41 +0400

    logs features

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -6,7 +6,6 @@ const NavbarCentry = {
         'user', 'logo_url',
         'active_section', 'active_subsection',
         'active_project', 'active_mode', 'active_parameter',
-        'is_admin_user',
     ],
     template: `
 <nav class="navbar navbar-expand main-nav justify-content-between" style="position: fixed; top: 0; width: 100%; z-index: 1000;">
@@ -87,11 +86,7 @@ const NavbarCentry = {
     data() {
         return {
             projects: [],
-            isAdmin: false,
         }
-    },
-    async mounted() {
-        this.isAdmin = this.is_admin_user
     },
     computed: {
         is_project_mode() {
@@ -155,7 +150,7 @@ const NavbarCentry = {
             const currentSection = this.$refs.sectionSelect.value
 
             const withinSections = newSections.filter(section => section === currentSection).length > 0
-            if (!withinSections && newSections.length > 0 && !this.isAdmin) {
+            if (!withinSections && newSections.length > 0) {
                 const newSection = newSections[0]
                 location.href = this.get_section_href(newSection)
                 this.$refs.sectionSelect.value = newSection
