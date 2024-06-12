12370c5033a98b5a2095eb6848372457856a9739
Vitalii_Kondratskyi <kondratskivd@gmail.com>
2024-06-12 19:41:47 +0300

    feat(DARK MODE): added dark mode;

--- static/js/locations.js
+++ static/js/locations.js
@@ -351,7 +351,7 @@ const Locations = {
     </div>
     <div class="d-flex py-4">
         <div class="custom-input w-100-imp displacement-ml-4">
-            <p class="custom-input_desc font-semibold mb-1">Engine location</p>
+            <p class="custom-input_desc font-semibold mb-1 font-h5">Engine location</p>
             <select class="selectpicker bootstrap-select__b" data-style="btn"
                 v-model="location_"
             >
@@ -367,7 +367,7 @@ const Locations = {
             </select>
         </div>
         <div class="custom-input ml-3">
-            <p class="custom-input_desc font-semibold mb-1">Runners</p>
+            <p class="custom-input_desc font-semibold mb-1 font-h5">Runners</p>
             <input-stepper
                 :default-value="parallel_runners"
                 :uniq_id="modal_id + '_parallel'"
@@ -375,7 +375,7 @@ const Locations = {
             ></input-stepper>
         </div>
         <div class="custom-input ml-3">
-            <p class="custom-input_desc font-semibold mb-1">CPU Cores</p>
+            <p class="custom-input_desc font-semibold mb-1 font-h5">CPU Cores</p>
             <input-stepper
                 :default-value="cpu"
                 :uniq_id="modal_id + '_cpu'"
@@ -383,7 +383,7 @@ const Locations = {
             ></input-stepper>
         </div>
         <div class="custom-input mx-3">
-            <p class="custom-input_desc font-semibold mb-1">Memory, Gb</p>
+            <p class="custom-input_desc font-semibold mb-1 font-h5">Memory, Gb</p>
             <input-stepper
                 :default-value="memory"
                 :uniq_id="modal_id + '_memory'"
@@ -531,7 +531,7 @@ const UILocations = {
     </div>
     <div class="d-flex py-4">
         <div class="custom-input w-100-imp displacement-ml-4">
-            <p class="custom-input_desc font-semibold mb-1">Engine location</p>
+            <p class="custom-input_desc font-semibold mb-1 font-h5">Engine location</p>
             <select class="selectpicker bootstrap-select__b" data-style="btn"
                 v-model="location_"
             >
@@ -557,7 +557,7 @@ const UILocations = {
         </div>
     -->
         <div class="custom-input ml-3">
-            <p class="custom-input_desc font-semibold mb-1">CPU Cores</p>
+            <p class="custom-input_desc font-semibold mb-1 font-h5">CPU Cores</p>
             <input-stepper
                 :default-value="cpu"
                 :uniq_id="modal_id + '_cpu'"
@@ -565,7 +565,7 @@ const UILocations = {
             ></input-stepper>
         </div>
         <div class="custom-input mx-3">
-            <p class="custom-input_desc font-semibold mb-1">Memory, Gb</p>
+            <p class="custom-input_desc font-semibold mb-1 font-h5">Memory, Gb</p>
             <input-stepper
                 :default-value="memory"
                 :uniq_id="modal_id + '_memory'"
